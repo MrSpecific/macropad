117ec62c7d6016e6d527c6a27682c2a72cd37560
Will Christenson <will@wlcr.io>
2022-06-08 14:35:06 -0700

    Updates

--- macros/dev.py
+++ macros/dev.py
@@ -13,19 +13,20 @@ app = {  # REQUIRED dict, must be named 'app'
         # 1st row ----------
         (0x105006, "Code", ["code .", Keycode.ENTER]),
         (0x105006, "Open", ["open .", Keycode.ENTER]),
-        (0x930462, "Goto", [Keycode.COMMAND, "P"]),  # Go to file
+        (0x002811, "Goto", [Keycode.COMMAND, "P"]),  # Go to file
         # 2nd row ----------
         (0x483502, "< Tab", [Keycode.COMMAND, Keycode.ALT, Keycode.LEFT_ARROW]),
         (0x483502, "Tab >", [Keycode.COMMAND, Keycode.ALT, Keycode.RIGHT_ARROW]),
-        (0x930462, "Cmd", [Keycode.COMMAND, Keycode.SHIFT, "P"]),  # Command Pallete
+        (0x002811, "Cmd", [Keycode.COMMAND, Keycode.SHIFT, "P"]),  # Command Pallete
         # 3rd row ----------
         (0x082C57, "Status", ["git", " ", "status", Keycode.ENTER]),
         (0x002531, "Remote", ["git", " ", "remote -v", Keycode.ENTER]),
-        (0x002531, "Checko", ["git", " ", "checkout "]),
+        # (0x002531, "Checko", ["git", " ", "checkout "]),
+        (0x1a0026, "Pull", ["git pull", Keycode.ENTER]),  # Hack-a-Day in new win
         # 4th row ----------
         (0x8315B7, "Add", ["git add ."]),  # Adafruit in new window
         (0x8315B7, "Commit", ['git commit -a -m ""', Keycode.LEFT_ARROW]),
-        (0x8315B7, "Push", ["git push"]),  # Hack-a-Day in new win
+        (0x1a0026, "Push", ["git push", Keycode.ENTER]),  # Hack-a-Day in new win
         # Encoder button ---
         (0x000000, "", [Keycode.COMMAND, "w"]),  # Close window/tab
     ],
